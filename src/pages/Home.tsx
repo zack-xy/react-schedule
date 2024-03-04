8b5d214d84981303a09e53f9ff1b5157d0f0737d
zack zheng <zack_zhengxiyun@163.com>
2024-03-04 22:01:10 +0800

    WIP: update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,27 @@
 import type { FC } from 'react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'antd'
+import { Button, Typography } from 'antd'
+import { MANAGE_INDEX_PATHNAME } from '../router'
+import styles from './Home.module.scss'
+
+const { Title, Paragraph } = Typography
 
 const Home: FC = () => {
   const nav = useNavigate()
 
-  function clickHandler() {
-    nav('/login')
-  }
+  // function clickHandler() {
+  //   nav('/login')
+  // }
 
   return (
-    <div>
-      <p>Home</p>
-      <div>
-        <Button type="primary" onClick={clickHandler}>登陆</Button>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 ｜ 在线投票</Title>
+        <Paragraph>已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980份</Paragraph>
+        <div>
+          <Button type="primary" onClick={() => nav(MANAGE_INDEX_PATHNAME)}>开始使用</Button>
+        </div>
       </div>
     </div>
   )
