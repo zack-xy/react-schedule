1d85ee18b4ca939fbc17f66b78cb288e9d37b164
zack zheng <zack_zhengxiyun@163.com>
2024-03-12 22:31:18 +0800

    WIP: update

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,1 +1,11 @@
+// 搜素参数
 export const LIST_SEARCH_PARAM_KEY = 'keyword'
+
+// 翻页参数 - 第几页
+export const LIST_PAGE_PARAM_KEY = 'page'
+
+// 翻页参数 - 每页多少条
+export const LIST_PAGE_SIZE_PARAM_KEY = 'pageSize'
+
+// 默认的每页条数
+export const LIST_PAGE_SIZE = 10
--- src/hooks/useLoadQuestionListData.ts
+++ src/hooks/useLoadQuestionListData.ts
@@ -1,7 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 import { useRequest } from 'ahooks'
 import { getQuestionListService } from '../services/question'
-import { LIST_SEARCH_PARAM_KEY } from '../constant'
+import { LIST_PAGE_PARAM_KEY, LIST_PAGE_SIZE, LIST_PAGE_SIZE_PARAM_KEY, LIST_SEARCH_PARAM_KEY } from '../constant'
 
 interface OptionType {
   isStar: boolean
@@ -11,12 +11,12 @@ interface OptionType {
 function useLoadQuestionListData(opt: Partial<OptionType> = {}) {
   const { isStar, isDeleted } = opt
   const [searchParams] = useSearchParams()
-  // eslint-disable-next-line no-console
-  console.log('keyword', searchParams.get('keyword'))
   const { data, loading, error } = useRequest(async () => {
     const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+    const page = Number.parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || '') || 1
+    const pageSize = Number.parseInt(searchParams.get(LIST_PAGE_SIZE_PARAM_KEY) || '') || LIST_PAGE_SIZE
 
-    const data = await getQuestionListService({ keyword, isStar, isDeleted })
+    const data = await getQuestionListService({ keyword, isStar, isDeleted, page, pageSize })
     return data
   }, { refreshDeps: [searchParams] })
 
